cb490e093115d68c3f69d458fe5b5655ce09336d
Zenjutahi <zenjutahi@gmail.com>
2018-11-13 17:09:02 +0300

    [feature] setup article destory

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -35,6 +35,13 @@ class ArticlesController < ApplicationController
     end
   end
   
+  def destory
+    @article = Article.find(params[:id])
+    @article.destory
+    
+    redirect_to articles_path
+  end
+  
   private
     def article_params
       params.require(:article).permit(:title, :text)
