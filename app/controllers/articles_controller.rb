a0a6b877cabcee4589e567d772341dda6a584a06
Zenjutahi <zenjutahi@gmail.com>
2018-11-13 12:21:53 +0300

    [bug] fix private method configuration

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,14 +7,14 @@ class ArticlesController < ApplicationController
   end
   
   def create
-    @article = Article.new(params[:article])
+    @article = Article.new(article_params)
     
     @article.save
     redirect_to @article
-    
-    private
-      def article_params
-        params.require(:article).permit(:title, :text)
-      end 
   end
+  
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end 
 end
